5f903010b6401e3fa56cb5e1750821476b62f1dd
OdiSeU <secret36565@gmail.com>
2021-06-17 10:57:39 +0900

    project 1차 complete

--- js/edit_n_view.js
+++ js/edit_n_view.js
@@ -1,7 +1,10 @@
 function openTest(editor) {
     let doc = document.getElementById(editor).value;
+    let script = document.createElement('script');
+    script.text = 'window.onblur = function(){window.close()};'
     testWindow = window.open('','result',"width=400, height=400, toolbar=no, menubar=no, scrollbars=no, resizable=yes");
     testWindow.document.head.innerHTML = '';
     testWindow.document.body.innerHTML = '';
     testWindow.document.write(doc);
+    testWindow.document.head.append(script);
 }
